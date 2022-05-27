f43ac147bd04f9a238a06a99f18600456fe22635
aleksandr_zelenskiy <dezion777@gmail.com>
2022-05-28 00:04:47 +0400

    added login flow from localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {AppRouter} from "./components/AppRouter";
 import {Layout} from "antd";
 import {Navbar} from "./components/Navbar/Navbar";
 import './App.css'
+import {useActions} from "./hooks/useActions";
+import {IUser} from "./models/IUser";
 
 export const App = () => {
+  const {setAuth, setUser} = useActions()
+
+  useEffect(() => {
+    const isAuth = localStorage.getItem('auth');
+    const username = localStorage.getItem('username');
+
+    if (isAuth && username) {
+      setAuth(true);
+      setUser({username} as IUser)
+    }
+  },[setAuth, setUser])
+
     return (
             <Layout>
                 <Navbar />
